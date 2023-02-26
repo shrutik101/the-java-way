337fe3bfc0f0b0c5789fbdd96d1e7ac0d2fdcf11
Shruti Kulkarni <shruti.k101@gmail.com>
2023-02-26 10:21:56 -0500

    Created methods as per the comments.

--- src/main/java/qa/automation/java/utils/LargestNumber.java
+++ src/main/java/qa/automation/java/utils/LargestNumber.java
@@ -4,19 +4,28 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class LargestNumber {
-    public static void main(String[] args) {
+    static int[] randomNumbers(int count, int upperBound){
         Random rand = new Random();
-        int[] numbers = new int[10];
+        int[] numbers = new int[count];
         for (int i=0; i<numbers.length; i++) {
-            numbers[i] = rand.nextInt(1000);
+            numbers[i] = rand.nextInt(upperBound);
         }
+        return numbers;
+    }
+
+    static int findLargestNumber(int[] numbers){
         int max = numbers[0];
         for (int i : numbers) {
             if (i > max) {
                 max = i;
             }
         }
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] numbers = randomNumbers(10,1000);
+        int maxNumber = findLargestNumber(numbers);
         System.out.println("The selected numbers are: " + Arrays.toString(numbers));
-        System.out.println("The largest number is: " + max);
+        System.out.println("The largest number is: " + maxNumber);
     }
 }
