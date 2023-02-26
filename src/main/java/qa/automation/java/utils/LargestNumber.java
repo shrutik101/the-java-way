5f1a1a6d9e869c87e52779b6fa64752624832ed0
Shruti Kulkarni <shruti.k101@gmail.com>
2023-02-25 22:54:17 -0500

    Created utility

--- src/main/java/qa/automation/java/utils/LargestNumber.java
+++ src/main/java/qa/automation/java/utils/LargestNumber.java
@@ -1,7 +1,22 @@
 package qa.automation.java.utils;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class LargestNumber {
     public static void main(String[] args) {
-        System.out.println("Hello world!!");
+        Random rand = new Random();
+        int[] numbers = new int[10];
+        for (int i=0; i<numbers.length; i++) {
+            numbers[i] = rand.nextInt(1000);
+        }
+        int max = numbers[0];
+        for (int i : numbers) {
+            if (i > max) {
+                max = i;
+            }
+        }
+        System.out.println("The selected numbers are: " + Arrays.toString(numbers));
+        System.out.println("The largest number is: " + max);
     }
 }
